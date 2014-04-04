fbf0267a65eefd579451e2ded708b24b6bf32d48
Charlie Whitney <charlie.whitney@gmail.com>
2014-04-04 13:22:33 -0400

    Updated vc project

---
+++ samples/BasicSample/blocks/PretzelGUI/src/BasePretzel.h
@@ -0,0 +1,46 @@
+//
+//  BasePretzel.h
+//  BasicSample
+//
+//  Created by Charlie Whitney on 2/4/14.
+//
+//
+
+#pragma once
+
+#include "cinder/app/AppNative.h"
+#include <boost/algorithm/string.hpp>
+#include "PretzelGlobal.h"
+
+enum PretzelFillStyle {
+    FILL = 0
+};
+
+class BasePretzel {
+  public:
+    BasePretzel();
+  
+    virtual void draw(){};
+	virtual void registerPretzel(BasePretzel *Pretzel);
+    
+    virtual void updateBounds( const ci::Vec2f &offset, const ci::Rectf &parentBounds );
+    
+    virtual ci::Rectf getBounds();
+    virtual float getWidth();
+    virtual float getHeight();
+
+	virtual void mouseDown(const ci::Vec2i &pos);
+	virtual void mouseDragged(const ci::Vec2i &pos);
+	virtual void mouseUp(const ci::Vec2i &pos);
+    
+  protected:
+    virtual void updateChildrenBounds();
+
+	ci::Vec2f                   mOffset;
+	ci::Rectf                   mBounds;
+
+	std::vector<BasePretzel*>     mPretzelChildren;
+    ci::Rectf                   mParentBounds;
+
+	Pretzel::PretzelGlobal			*mGlobal;
+};
--- samples/BasicSample/src/BasicSampleApp.cpp
+++ samples/BasicSample/src/BasicSampleApp.cpp
@@ -2,23 +2,20 @@
 #include "cinder/gl/gl.h"
 #include "cinder/Rand.h"
 
-#include "PanelGui.h"
-#include "PanelRow.h"
-#include "PanelLabel.h"
+#include "PretzelGui.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class BasicSampleApp : public AppNative {
-  public:
-    void prepareSettings( Settings *settings );
+public:
+	void prepareSettings(Settings *settings);
 	void setup();
 	void update();
 	void draw();
-    
-    PanelGui    *gui;
-    PanelRow    *titleBar, *mainBar;
+
+	PretzelGui    *gui;
 
 	float		mRadius;
 	float		mOpacity;
@@ -27,22 +24,20 @@ class BasicSampleApp : public AppNative {
 	void onButtonPress();
 };
 
-void BasicSampleApp::prepareSettings( Settings *settings ) {
-    settings->setWindowSize( Vec2i(1024, 768) );
+void BasicSampleApp::prepareSettings(Settings *settings) {
+	settings->setWindowSize(Vec2i(1024, 768));
 	settings->setFrameRate(60.0);
 	settings->enableConsoleWindow();
 }
 
 void BasicSampleApp::setup() {
 	Rand::randomize();
-    
+
 	mRadius = 25;
 	mOpacity = 1.0;
 	bDrawOutline = false;
 
-    //gui = new PanelGui( PanelFillStyle::FILL, PanelFillStyle::FILL );
-	gui = new PanelGui();
-    gui->setup();
+	gui = new PretzelGui("My settings");
 	gui->addSlider("Opacity", &mOpacity, 0, 1);
 	gui->addSlider("Radius", &mRadius, 0, 100);
 	gui->addLabel("Other Settings");
@@ -51,11 +46,11 @@ void BasicSampleApp::setup() {
 	gui->addToggle("Draw outline", &bDrawOutline);
 
 	//titleBar = new PanelRow(gui, PanelFillStyle::FILL, PanelFillStyle::FILL);
-    //mainBar = new PanelRow( gui, 200, PanelFillStyle::FILL );
+	//mainBar = new PanelRow( gui, 200, PanelFillStyle::FILL );
 }
 void BasicSampleApp::onButtonPress(){
 	console() << "Got a button press event" << endl;
-	gui->setSize( Vec2f(200, 400) );
+	gui->setSize(Vec2f(200, 400));
 
 }
 
@@ -65,18 +60,17 @@ void BasicSampleApp::update() {
 }
 
 void BasicSampleApp::draw() {
-	gl::clear( Color( 84./255.,166./255., 1 ) );
-    gl::color( ColorA(1,1,1, mOpacity) );
-    
+	gl::clear(Color(84. / 255., 166. / 255., 1));
+	gl::color(ColorA(1, 1, 1, mOpacity));
+
 	if (bDrawOutline){
 		gl::drawStrokedCircle(getWindowCenter(), mRadius);
 	}
 	else{
 		gl::drawSolidCircle(getWindowCenter(), mRadius);
 	}
-	//console() << "VALUE " << testSliderValue << endl;
 
-    gui->draw();
+	gui->draw();
 }
 
-CINDER_APP_NATIVE( BasicSampleApp, RendererGl )
+CINDER_APP_NATIVE(BasicSampleApp, RendererGl)
