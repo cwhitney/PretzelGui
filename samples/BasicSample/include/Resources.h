ac81052d6c37ec1517baf7a75c59db87cf4005ae
Charlie Whitney <charlie@sharkbox.com>
2015-12-29 21:22:27 -0500

    Fixed uninitialized vars, windows examples and rc files

--- samples/BasicSample/include/Resources.h
+++ samples/BasicSample/include/Resources.h
@@ -8,4 +8,4 @@
 
 #define PRETZEL_GUI_SKIN		CINDER_RESOURCE( ../../../resources/, default_skin.png, 128, IMAGE )
 #define PRETZEL_COLOR		CINDER_RESOURCE( ../../../resources/, color-picker.png, 129, IMAGE )
-#define PRETZEL_HSL_BAR		CINDER_RESOURCE( ../blocks/PretzelGUI/resources/, hslBar.png, 130, IMAGE )
+#define PRETZEL_HSL_BAR		CINDER_RESOURCE( ../../../resources/, hslBar.png, 130, IMAGE )
--- samples/MultiGuiSample/include/Resources.h
+++ samples/MultiGuiSample/include/Resources.h
@@ -3,9 +3,6 @@
 
 //#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
 
-
-
-
 #define PRETZEL_GUI_SKIN		CINDER_RESOURCE( ../../../resources/, default_skin.png, 128, IMAGE )
 #define PRETZEL_COLOR		CINDER_RESOURCE( ../../../resources/, color-picker.png, 129, IMAGE )
-#define PRETZEL_HSL_BAR		CINDER_RESOURCE( ../blocks/PretzelGUI/resources/, hslBar.png, 130, IMAGE )
+#define PRETZEL_HSL_BAR		CINDER_RESOURCE( ../../../resources/, hslBar.png, 130, IMAGE )
